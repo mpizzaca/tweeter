9318cb8b6fbccd302e04ffa0252dd210c3ab8d79
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-23 16:25:51 -0400

    fix header so it stays in place as user scrolls down page

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,4 @@
 $(document).ready(() => {
-
     $('textarea#tweet-text').on('input', function() {
       const textAreaLength = $(this).val().length;
       const remainingCharsOutput = $(this).siblings('div').children('.counter');
@@ -7,7 +6,7 @@ $(document).ready(() => {
       // update number of characters remaining
       remainingCharsOutput.val(140 - textAreaLength);
 
-      // add/remove the 'negative' class to style appropriately
+      // add/remove the 'negative' class to style counter appropriately
       if (140 - textAreaLength < 0) {
         remainingCharsOutput.addClass('negative');
       } else {
