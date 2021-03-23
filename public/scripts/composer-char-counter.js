c30a15b09f985f6374e1b3924b5251602ecd3197
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-23 13:42:35 -0400

    add 'remaining characters' counter to tweet textarea

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+
+    $('textarea#tweet-text').on('input', function() {
+      const textAreaLength = $(this).val().length;
+      const remainingCharsOutput = $(this).siblings('div').children('.counter');
+      
+      // update number of characters remaining
+      remainingCharsOutput.val(140 - textAreaLength);
+
+      // add/remove the 'negative' class to style appropriately
+      if (140 - textAreaLength < 0) {
+        remainingCharsOutput.addClass('negative');
+      } else {
+        remainingCharsOutput.removeClass('negative');
+      }
+    });
+});
