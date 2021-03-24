db940d7423b395b8b56047600c5fd4554b48f877
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-24 14:53:55 -0400

    fix indentation

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,16 @@
 $(document).ready(() => {
-    $('textarea#tweet-text').on('input', function() {
-      const textAreaLength = $(this).val().length;
-      const remainingCharsOutput = $(this).siblings('div').children('.counter');
+  $('textarea#tweet-text').on('input', function() {
+    const textAreaLength = $(this).val().length;
+    const remainingCharsOutput = $(this).siblings('div').children('.counter');
       
-      // update number of characters remaining
-      remainingCharsOutput.val(140 - textAreaLength);
+    // update number of characters remaining
+    remainingCharsOutput.val(140 - textAreaLength);
 
-      // add/remove the 'negative' class to style counter appropriately
-      if (140 - textAreaLength < 0) {
-        remainingCharsOutput.addClass('negative');
-      } else {
-        remainingCharsOutput.removeClass('negative');
-      }
-    });
+    // add/remove the 'negative' class to style counter appropriately
+    if (140 - textAreaLength < 0) {
+      remainingCharsOutput.addClass('negative');
+    } else {
+      remainingCharsOutput.removeClass('negative');
+    }
+  });
 });
